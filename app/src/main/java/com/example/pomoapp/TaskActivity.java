09283ac68661813a80650f5938ddc3f2ca6ebb2e
michaelrosado <mrosad4@illinois.edu>
2019-12-10 14:03:49 -0600

    Added the list component and notifications

--- app/src/main/java/com/example/pomoapp/TaskActivity.java
+++ app/src/main/java/com/example/pomoapp/TaskActivity.java
@@ -1,39 +1,58 @@
 package com.example.pomoapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 public class TaskActivity extends AppCompatActivity {
+    List<String> toDoList;
+
+    ListView listView;
+
+    ArrayAdapter<String> arrayAdapter;
+    EditText input;
+    Button add;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.task_activity);
+        add = findViewById(R.id.addButton);
 
-        final Button remove = findViewById(R.id.RemoveTask);
-        final Button add = findViewById(R.id.AddTask);
-        remove.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TextView text = findViewById(R.id.listOfTasks);
-                text.setText("Task removed");
-            }
-        });
         add.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                addTask();
+            public void onClick(View v) {
+                addItem();
             }
         });
+        toDoList = new ArrayList<>();
+
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.list_layout, toDoList);
+        listView = findViewById(R.id.listView);
+
+        listView.setAdapter(arrayAdapter);
+        input = findViewById(R.id.input);
 
     }
-    public void addTask() {
-        Intent intent = new Intent(this, AddTaskActivity.class);
-        startActivity(intent);
+
+    public void addItem() {
+        toDoList.add(input.getText().toString());
+        arrayAdapter.notifyDataSetChanged();
+
+        input.setText("");
     }
+
 }
--- app/src/main/java/com/example/pomoapp/pomoActivity.java
+++ app/src/main/java/com/example/pomoapp/pomoActivity.java
@@ -1,20 +1,44 @@
 package com.example.pomoapp;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.UrlQuerySanitizer;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.PersistableBundle;
+import android.os.StrictMode;
+import android.renderscript.ScriptGroup;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationBuilderWithBuilderAccessor;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Locale;
 
 public class pomoActivity extends AppCompatActivity {
+    private final String CHANNEL_ID = "Channel1";
+    private final int NOTIFICATION_ID = 001;
     // 25 minutes to milliseconds
-    private static final long START_TIME_IN_MILL = 1500000;
+    private static final long START_TIME_IN_MILL = 12000;
 
     private TextView textViewCountdown;
     private Button buttonStartPause;
@@ -22,6 +46,9 @@ public class pomoActivity extends AppCompatActivity {
     private CountDownTimer countDownTImer;
     private boolean timerRunning;
     private long timeLeftInMill = START_TIME_IN_MILL;
+    private NotificationManagerCompat notificationManager;
+
+    private static HttpURLConnection connection;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,6 +57,7 @@ public class pomoActivity extends AppCompatActivity {
         buttonStartPause = findViewById(R.id.startPomo);
         buttonReset = findViewById(R.id.resetPomo);
         textViewCountdown = findViewById(R.id.text_view_countdown);
+
 
         buttonStartPause.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,9 +76,19 @@ public class pomoActivity extends AppCompatActivity {
             }
         });
         updateCountDownText();
+        notificationManager = NotificationManagerCompat.from(this);
+        createNotificationChannels();
 
     }
 
+    private static String requestQuote() throws Exception {
+        URL url = new URL("https://quotes.rest/qod?category=inspire");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        InputStream input = connection.getInputStream();
+        String quote = input.toString();
+        return quote;
+    }
     private void startTimer() {
        countDownTImer = new CountDownTimer(timeLeftInMill, 1000) {
            @Override
@@ -62,7 +100,10 @@ public class pomoActivity extends AppCompatActivity {
            @Override
            public void onFinish() {
                 timerRunning = false;
-                buttonReset.setText("Resume");
+                buttonReset.setText("Reset");
+                buttonStartPause.setText("Start");
+                // set up notification here.
+               sendNotificationChannel();
 
            }
        }.start();
@@ -79,7 +120,6 @@ public class pomoActivity extends AppCompatActivity {
         timeLeftInMill = START_TIME_IN_MILL;
         updateCountDownText();
 
-
     }
     private void updateCountDownText() {
         int minute = (int) (timeLeftInMill / 1000) / 60;
@@ -87,6 +127,31 @@ public class pomoActivity extends AppCompatActivity {
 
         String timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d", minute, seconds);
         textViewCountdown.setText(timeLeftFormatted);
+
+    }
+    private void sendNotificationChannel() {
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_timer_notification)
+                .setContentTitle("Timer is complete")
+                .setContentText("take a quick break, you deserve it!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManager.notify(1, notification);
+    }
+    private void createNotificationChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel1 = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Channel 1",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            channel1.setDescription("this is channel 1");
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel1);
+
+        }
 
     }
 }
